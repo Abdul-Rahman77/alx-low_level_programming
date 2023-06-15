1b3bb1db8ca4d4949a7285f43a8cb2caa4c34bda
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-06-15 22:17:58 +0000

    Readmes and project folders created

---
+++ 0x00-hello_world/c_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read -p "Filename: " fname
+echo -e "#!/bin/bash/n" > $fname
+vi $fname
+chmod u+x $fname
+./$fname
+git add .
+read -p "Commit message: " msg
+echo "$fname - $msg" >> README.md
+git commit -m "$msg"
+git push origin master
+clear
