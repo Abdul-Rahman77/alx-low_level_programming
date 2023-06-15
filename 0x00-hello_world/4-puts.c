c0224002c2c151f379acf8adab51ffb6fe92e7bb
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-06-15 23:51:14 +0000

    C program that prints the size of various types on the computer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
-*main - prints a string
-*
-*return 0 if program exits as expected
-*/
-int main(void) 
+   * main - prints the phrase "Programming is like building a multilingual puzzle
+    * Return: 0 if exited properly, non-zero otherwise
+     */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
--- 0x00-hello_world/p_script.sh
+++ 0x00-hello_world/p_script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 read -p "Filename: " fname
-cp index.c $fname
 vi $fname
 read -p "Output filename: " ofile
 gcc $fname -o $ofile
