6499fad4a694984235e2204938ed29d1a1529e36
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-06-16 00:54:01 +0000

    Advanced task correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
 * main - Entry point
 *
-* return: Always 1 (Error)
+* Return: Always 1 (Error)
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"", stdout);
+	fputs("- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
