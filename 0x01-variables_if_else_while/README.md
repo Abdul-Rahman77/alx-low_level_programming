b8530c8882c1e4575f5af73926c0f2f58894fa20
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-06-16 21:03:21 -0400

    This program will assign a random number to the variable n each time it is executed.

---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+#Description of all files in this project
+
---
+++ 0x01-variables_if_else_while/c_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -p "Filename: " fname
+vi $fname.c
+gcc -Wall -Werror -Wextra -pedantic -std=gnu89 $fname.c -o $fname.exe
+./$fname.exe
+
+git add .
+read -p "Commit message: " msg
+echo -e "#$fname.c - $msg" > README.md
+git commit -m "$msg"
+git push origin master
+
+clear
+./c_files.sh
+
+
