e0fff9a8c5d8ddc8dc32ebc8bcc06ba0ef072ff3
Abdul Rahman <yhungmhansuccessor@gmail.com>
2023-06-30 01:51:38 -0400

    All in one

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _putchar - prints a character to stdout
+* @c: argument of character type to be returned by _putchar
+*/
+
+int _putchar(char *c)
+{
+	return (write(1, &c, 1);)
+}
+
